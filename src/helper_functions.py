ed5b92da1aa60d142ff23015c64dd4b889b0cec7
Sidney Kung <sidneyjkung@gmai.com>
2020-12-29 15:38:08 -0500

    cross_val_score all implimented, analysis up next

--- src/helper_functions.py
+++ src/helper_functions.py
@@ -1,3 +1,4 @@
+# importing necessary functions
 import numpy as np
 import pandas as pd
 import pickle
@@ -20,25 +21,41 @@ from sklearn import metrics
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix, plot_confusion_matrix, roc_curve, auc, classification_report
 from sklearn.model_selection import cross_val_score
 
+
 ## functions used in data_cleaning.ipynb
 
-# tweet cleaning fucntion
+# tweet cleaning function
 def clean_text_round1(text):
     '''Make text lowercase, remove punctuation, mentions, hashtags and words containing numbers.'''
+    # make text lowercase
     text = text.lower()
+    # removing text within brackets
     text = re.sub('\[.*?\]', '', text)
+    # removing text within parentheses
     text = re.sub('\(.*?\)', '', text)
+    # removing numbers
     text = re.sub('\w*\d\w*', '', text)
+    # if there's more than 1 whitespace, then make it just 1
     text = re.sub('\s+', ' ', text)
+    # if there's a new line, then make it a whitespace
     text = re.sub('\n', ' ', text)
+    # removing any quotes
     text = re.sub('\"+', '', text)
+    # removing &amp;
     text = re.sub('(\&amp\;)', '', text)
+    # removing any usernames
     text = re.sub('(@[^\s]+)', '', text)
+    # removing any hashtags
     text = re.sub('(#[^\s]+)', '', text)
+    # remove `rt` for retweet
     text = re.sub('(rt)', '', text)
+    # string.punctuation is a string of all punctuation marks
+    # so this gets rid of all punctuation
     text = re.sub('[%s]' % re.escape(string.punctuation), '', text)
+    # getting rid of `httptco`
     text = re.sub('(httptco)', '', text)
     return text
+
 
 ## functions used in nlp_preprocessing.ipynb
 
@@ -47,12 +64,14 @@ def unfiltered_tokens(text):
     dirty_tokens = nltk.word_tokenize(text)
     return dirty_tokens
 
+# tokenizing and removing stop words
 def process_tweet(text):
     """tokenize text in each column and remove stop words"""
     stop_words = set(stopwords.words('english'))
     tokens = nltk.word_tokenize(text)
     stopwords_removed = [token.lower() for token in tokens if token.lower() not in stop_words]
     return stopwords_removed 
+
 
 ## functions used for modeling
 
@@ -69,21 +88,34 @@ def evaluation(precision, recall, f1_score, f1_weighted):
 def train_cross_validation(model, X_train, y_train, metric, x):
     """prints cross-validation TRAINING metrics for a model"""
     scores = cross_val_score(model, X_train, y_train, scoring=metric, cv=x)
-    print('Cross-Validation F1 Scores:', scores)    
+    print('Cross-Validation F1 Scores on Training Set:', scores)    
     print('\nMin: ', round(scores.min(), 6))
     print('Max: ', round(scores.max(), 6))
     print('Mean: ', round(scores.mean(), 6)) 
     print('Range: ', round(scores.max() - scores.min(), 6))
 
 # determing out whether the model overfit or underfit
-def fit(cv_f1, f1_score):
-    if cv_f1 > f1_score:
-        fit = 'overfit'
+def model_fit(cv_train_metric, test_metric):
+    if cv_train_metric > test_metric:
+        models_fit = 'overfit'
     else:
-        fit = 'underfit'
-    return fit
+        models_fit = 'underfit'
+    return models_fit
 
-# Doc2Vec
+
+## Doc2Vec Functions
+
+# tokenizing specifically for Doc2Vec
+def tokenize_text(text):
+    """tokenize and remove stop words with NLTK"""
+    tokens = []
+    for sent in nltk.sent_tokenize(text):
+        for word in nltk.word_tokenize(sent):
+            if len(word) < 2:
+                continue
+            tokens.append(word.lower())
+    return tokens
+
 def vec_for_learning(model, tagged_docs):    
     """final vector feature for classifier use"""
     sents = tagged_docs.values
